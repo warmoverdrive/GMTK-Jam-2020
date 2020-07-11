28eb609f5a1fae6d264bc7ca63c84c1a5bd39ca7
warmoverdrive <sweetsthebear@gmail.com>
2020-07-10 21:03:54 -0500

    Set up tool assets and animations

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetTrigger("bounce");
+    }
+}
--- Assets/Scripts/SpringPad.cs
+++ Assets/Scripts/SpringPad.cs
@@ -6,12 +6,21 @@ public class SpringPad : MonoBehaviour
 {
     [SerializeField] float springForce = 10f;
 
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         var otherRigidbody = other.GetComponent<Rigidbody2D>();
         if (!otherRigidbody) return;
 
+        otherRigidbody.velocity = new Vector2(otherRigidbody.velocity.x, 0);
         otherRigidbody.AddForce(new Vector2(0, springForce));
+        animator.SetTrigger("spring");
     }
 }
